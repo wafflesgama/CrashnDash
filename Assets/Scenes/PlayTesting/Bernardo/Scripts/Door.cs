6344654d1e9ac0c8b50b180411753eefe930d48d
Guilherme Gama <guilherme.s.gam@gmail.com>
2022-02-18 16:27:12 +0000

    Added new doors and new NPC bases

--- Assets/Scenes/PlayTesting/Bernardo/Scripts/Door.cs
+++ Assets/Scenes/PlayTesting/Bernardo/Scripts/Door.cs
@@ -3,19 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Sirenix.OdinInspector;
-
+using DG.Tweening;
 
 public class Door : MonoBehaviour
 {
-    public Animator doorLeft;
-    public Animator doorRight;
+    //public Animator doorLeft;
+    //public Animator doorRight;
+    public float openDistance = 4;
 
+    public float openSpeed = 1;
+    public float closeSpeed = 1;
+
+    public Ease openAnimEase;
+    public Ease closeAnimEase;
+
+    public Transform doorLeft;
+    public Transform doorRight;
+
+    float rightInitPos;
+    float leftInitPos;
 
     bool open = false;
     void Start()
     {
-        doorLeft.SetBool("isLeft", true);
-        doorRight.SetBool("isLeft", false);
+        rightInitPos = doorRight.localPosition.z;
+        leftInitPos = doorLeft.localPosition.z;
+        //doorLeft.SetBool("isLeft", true);
+        //doorRight.SetBool("isLeft", false);
     }
 
     public void OpenClose()
@@ -31,15 +45,19 @@ public class Door : MonoBehaviour
     [Button("Open Door")]
     public void OpenDoor()
     {
-        doorLeft.SetTrigger("OpenDoor");
-        doorRight.SetTrigger("OpenDoor");
+        doorLeft.DOLocalMoveZ(leftInitPos - openDistance, openSpeed).SetEase(openAnimEase);
+        doorRight.DOLocalMoveZ(rightInitPos + openDistance, openSpeed).SetEase(openAnimEase);
+        //doorLeft.SetTrigger("OpenDoor");
+        //doorRight.SetTrigger("OpenDoor");
     }
 
     [Button("Close Door")]
     public void CloseDoor()
     {
-        doorLeft.SetTrigger("CloseDoor");
-        doorRight.SetTrigger("CloseDoor");
+        doorLeft.DOLocalMoveZ(leftInitPos, closeSpeed).SetEase(closeAnimEase);
+        doorRight.DOLocalMoveZ(rightInitPos, closeSpeed).SetEase(closeAnimEase);
+        //doorLeft.SetTrigger("CloseDoor");
+        //doorRight.SetTrigger("CloseDoor");
     }
 
 }
